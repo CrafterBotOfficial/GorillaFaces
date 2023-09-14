34724740725d972ecba388fe0b99f52c4c4cc0ff
Crafterbot <118089496+CrafterBotOfficial@users.noreply.github.com>
2023-09-13 20:04:23 -0400

    Added error catching
    Since Photon invokes these methods via a foreach loop I want to make sure no exceptions are thrown that could break the loop.

--- GorillaFaces/GorillaFaces/Behaviours/Callbacks.cs
+++ GorillaFaces/GorillaFaces/Behaviours/Callbacks.cs
@@ -10,31 +10,36 @@ namespace GorillaFaces.Behaviours
         public override async void OnPlayerEnteredRoom(Player newPlayer)
         {
             await Task.Yield(); // Waiting for the VRRig to be pulled from the pool
-            FaceController.AttemptEquip(newPlayer);
+            try { FaceController.AttemptEquip(newPlayer); } 
+            catch { Main.Log("Error occured while attempting to equip face onto user.", BepInEx.Logging.LogLevel.Error); }
         }
 
         public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
         {
-            if (changedProps.TryGetValue(Main.PropertyKey, out object obj))
+            try 
             {
-                Main.Log("Got face update event for " + targetPlayer.NickName + " with value " + (string)obj, BepInEx.Logging.LogLevel.Message);
-                FaceController.EquipFace(targetPlayer, (string)obj);
+                if (changedProps.TryGetValue(Main.PropertyKey, out object obj))
+                {
+                    Main.Log("Got face update event for " + targetPlayer.NickName + " with value " + (string)obj, BepInEx.Logging.LogLevel.Message);
+                    FaceController.EquipFace(targetPlayer, (string)obj);
+                }
             }
+            catch (System.Exeption ex) { Main.Log("Error while checking for face change. " + ex, BepInEx.Logging.LogLevel.Error); }
         }
 
         public override async void OnJoinedRoom()
         {
-            await Task.Yield(); // Waiting for our local VRRig to be pulled from the pool
-            // We need to set the face for the online rig
-            // Main.Log("Joined room", BepInEx.Logging.LogLevel.Message);
-            FaceController.EquipFace(PhotonNetwork.LocalPlayer, Configuration.SelectedFace.Value);
-
-            // Now we need to equip the faces for all the other players
-            Player[] players = PhotonNetwork.PlayerListOthers; // excludes the local player, and ordered by actor number
-            foreach (Player player in players)
+            try
             {
-                FaceController.AttemptEquip(player);
+                await Task.Yield(); // Waiting for our local VRRig to be pulled from the pool
+                // We need to set the face for the online rig
+                FaceController.EquipFace(PhotonNetwork.LocalPlayer, Configuration.SelectedFace.Value);
+
+                // Equip the faces for all the other players
+                Player[] players = PhotonNetwork.PlayerListOthers; 
+                foreach (Player player in players) FaceController.AttemptEquip(player);
             }
+            catch (System.Exeption ex) { Main.Log("Error while updating face for local player & others. " + ex, BepInEx.Logging.LogLevel.Error); }
         }
     }
 }
